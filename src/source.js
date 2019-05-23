908f85442e0ec00f59baca92b559394557bfceed
ValTM <valeri4ew@yahoo.com>
2019-05-23 04:12:09 +0300

    Functioning buttons, clear window button, styling

--- src/source.js
+++ src/source.js
@@ -7,15 +7,18 @@ class FbLinkCleaner {
     this.main = FbLinkCleaner.setupMainDiv();
     const headerDiv = FbLinkCleaner.setupHeaderDiv();
     this.setupDialogDrag(headerDiv);
-    const closeBtn = FbLinkCleaner.setupCloseBtn();
-    headerDiv.appendChild(closeBtn);
-    const bodyDiv = FbLinkCleaner.setupBodyDiv();
+    const bodyDiv = this.setupBodyDiv();
     const footerDiv = FbLinkCleaner.setupFooterDiv();
+    const clearAllBtn = this.setupClearAllBtn();
+    footerDiv.appendChild(clearAllBtn);
     this.main.append(headerDiv, bodyDiv, footerDiv);
-    FbLinkCleaner.registerEventListeners();
   }
 
-  static getOriginalLinkOnMiddleClick(e) {
+  /**
+   * Gets the original link from the facebook A element when clicked with the scroll wheel
+   * @param {MouseEvent} e
+   */
+  getOriginalLinkOnMiddleClick(e) {
     if (e.button === 1 && e.target.tagName === 'A') {
       e.preventDefault();
       const aHref = new URL(e.target.href);
@@ -26,16 +29,25 @@ class FbLinkCleaner {
       } else {
         origLink = FbLinkCleaner.generateOrigLink(aHref);
       }
-      console.log(origLink);
+      this.setupRow(origLink);
     }
   }
 
+  /**
+   * Cancels click event on middle click
+   * @param {MouseEvent} e
+   */
   static cancelClickOnMiddleClick(e) {
     if (e.button === 1) {
       e.preventDefault();
     }
   }
 
+  /**
+   * Generates the original link from an URL object
+   * @param {URL} url the URL to generate from
+   * @returns {string} the original link without fbclid
+   */
   static generateOrigLink(url) {
     let baseUrl = `${url.protocol}//${url.hostname}${url.pathname}`;
     if (url.search.lastIndexOf(fbclid) > 0) {
@@ -45,29 +57,195 @@ class FbLinkCleaner {
     return baseUrl;
   }
 
+  /**
+   * Sets up a row for the current link
+   * @param {string} href
+   */
+  setupRow(href) {
+    const rowWrapper = FbLinkCleaner.createRowWrapper();
+    const linkWrapper = FbLinkCleaner.createLinkWrapper(href);
+    const buttonsWrapper = FbLinkCleaner.createButtonsWrapper();
+    const copyButton = FbLinkCleaner.createRowButton('Copy to clipboard', 'COPY', FbLinkCleaner.getCopyLinkFn()
+      .bind({
+        href,
+        mainDiv: this.main,
+      }));
+    const openButton = FbLinkCleaner.createRowButton('Open in new tab', 'OPEN', FbLinkCleaner.getOpenLinkFn()
+      .bind(href));
+    const delButton = FbLinkCleaner.createRowButton('Delete row', 'X', FbLinkCleaner.getDeleteRowFn()
+      .bind({
+        bodydiv: this.bodydiv,
+        rowWrapper,
+      }));
+    buttonsWrapper.append(copyButton, openButton, delButton);
+    rowWrapper.appendChild(linkWrapper);
+    rowWrapper.appendChild(buttonsWrapper);
+    this.bodydiv.appendChild(rowWrapper);
+  }
+
+  /*
+   * BUTTON FUNCTIONS
+   */
+
+  /**
+   * Creats the function to copy the link to the clipboard
+   * @returns {Function}
+   */
+  static getCopyLinkFn() {
+    return function copyLinkFn() {
+      let textArea;
+      if (this.mainDiv.querySelector('textarea')) {
+        textArea = this.mainDiv.querySelector('textarea');
+      } else {
+        textArea = document.createElement('textarea');
+        textArea.style.cssText = 'opacity:0;width:0px;height:0px;margin:0;padding:0;border:none;box-sizing:border-box;';
+        this.mainDiv.appendChild(textArea);
+      }
+      textArea.value = this.href;
+      textArea.focus();
+      textArea.select();
+      document.execCommand('copy');
+    };
+  }
+
+  /**
+   * Creates a function to open the link in a new tab
+   * @returns {Function}
+   */
+  static getOpenLinkFn() {
+    return function openLink() {
+      window.open(this, '_blank');
+    };
+  }
+
+  /**
+   * Creates the function for deleting a row
+   * @returns {Function}
+   */
+  static getDeleteRowFn() {
+    return function deleteRow() {
+      this.bodydiv.removeChild(this.rowWrapper);
+    };
+  }
+
+  /*
+   * END BUTTON FUNCTIONS
+   */
+
+  /*
+  * ROW ELEMENTS HTML
+  */
+
+  /**
+   * Creates a button wrapper div
+   * @returns {HTMLDivElement}
+   */
+  static createButtonsWrapper() {
+    const buttonsWrapper = document.createElement('div');
+    buttonsWrapper.style.cssText = 'min-width:max-content';
+    return buttonsWrapper;
+  }
+
+  /**
+   * Creates a whole row wrapper div
+   * @returns {HTMLDivElement}
+   */
+  static createRowWrapper() {
+    const rowWrapper = document.createElement('div');
+    rowWrapper.style.cssText = 'background:aquamarine;display:flex;justify-content:space-between';
+    return rowWrapper;
+  }
+
+  /**
+   * Creates a link wrapper div
+   * @returns {HTMLDivElement}
+   */
+  static createLinkWrapper(href) {
+    const linkWrapper = document.createElement('div');
+    linkWrapper.style.cssText = 'padding-left:5px;overflow:hidden;color:black !important;display:flex;align-items:center';
+    linkWrapper.appendChild(FbLinkCleaner.createLink(href));
+    return linkWrapper;
+  }
+
+  /**
+   * Creates an A element with the href as link
+   * @param {string} href href for the A element
+   * @returns {HTMLAnchorElement} the created A element
+   */
+  static createLink(href) {
+    const link = document.createElement('a');
+    link.href = href;
+    link.innerText = href;
+    link.style.cssText = 'color:inherit;overflow:hidden;text-overflow:ellipsis;white-space:nowrap';
+    link.target = '_blank';
+    return link;
+  }
+
+  /**
+   * Creates a row button
+   * @param {string} bTitle title of the button
+   * @param {string} innerText inner text of the button
+   * @param {Function} handler a function to execute on click
+   * @returns {HTMLButtonElement} the created button
+   */
+  static createRowButton(bTitle, innerText, handler) {
+    const button = document.createElement('button');
+    button.style.cssText = 'border:1px solid;padding:2px 5px;cursor:pointer;margin:1px';
+    button.title = bTitle;
+    button.innerText = innerText;
+    button.onclick = handler;
+    return button;
+  }
+
+  /*
+  * END ROW ELEMENTS HTML
+  */
+
+  /*
+  * MAIN HTML ELEMENTS
+  */
+
+  /**
+   * Creates the footer div
+   * @returns {HTMLDivElement}
+   */
   static setupFooterDiv() {
     const footerDiv = document.createElement('div');
-    footerDiv.style.cssText = 'background:aqua;flex:0 1 10px';
+    footerDiv.style.cssText = 'flex:0 1 20px;display:flex;justify-content:flex-end;margin-right:12px;min-height:20px;';
     return footerDiv;
   }
 
-  static setupBodyDiv() {
+  /**
+   * Creates the body div
+   * @returns {HTMLDivElement}
+   */
+  setupBodyDiv() {
     const bodyDiv = document.createElement('div');
-    bodyDiv.style.cssText = 'background:aliceblue;flex:1 1 auto';
+    bodyDiv.style.cssText = 'flex:1 1 auto;display:flex;flex-direction:column;overflow:auto';
+    this.bodydiv = bodyDiv;
     return bodyDiv;
   }
 
+  /**
+   * Creates the header div
+   * @returns {HTMLDivElement}
+   */
   static setupHeaderDiv() {
     const headerDiv = document.createElement('div');
-    headerDiv.style.cssText = 'background:purple;flex:0 1 auto;display:flex;justify-content:flex-end;cursor:move';
+    headerDiv.style.cssText = ' flex:0 1 auto;display:flex;justify-content:flex-end;cursor:move;background-color:rgb(44,44,54)';
+    headerDiv.appendChild(FbLinkCleaner.setupCloseBtn());
     return headerDiv;
   }
 
+  /**
+   * Creates the close button for the dialog
+   * @returns {HTMLButtonElement}
+   */
   static setupCloseBtn() {
     const closeBtn = document.createElement('button');
     closeBtn.style.all = 'unset';
     closeBtn.title = 'Close';
-    closeBtn.style.cssText = 'border:none;padding:0 5px;background:transparent;cursor:pointer';
+    closeBtn.style.cssText = 'border:none;padding:0 5px;background:none;cursor:pointer;color:rgb(233,233,233);';
     closeBtn.innerText = 'X';
     closeBtn.onclick = () => {
       window.fblnkcln.hideDialog();
@@ -75,15 +253,40 @@ class FbLinkCleaner {
     return closeBtn;
   }
 
+  /**
+   * Creates the main div for the dialog
+   * @returns {HTMLDivElement}
+   */
   static setupMainDiv() {
     const mainDiv = document.createElement('div');
-    mainDiv.style.cssText = 'resize:both;overflow:auto;padding:0;marign:0;box-sizing:border-box;position:fixed;width:100px;height:100px;top:0;left:0;background:orange;z-index:1000000;display:flex;flex-direction:column';
+    mainDiv.style.cssText = 'resize:both;overflow:auto;padding:0px;box-sizing:border-box;position:fixed;width:250px;height:200px;top:0;left:0;background-color:rgb(35,35,45);z-index:1000000;display:flex;flex-direction:column;visibility:visible;opacity:0.7;transition: opacity 0.3s ease 0s, visibility 0.3s ease-in 0s;border:1px solid rgb(233,233,233);';
     mainDiv.id = 'fblnkclnid';
     document.querySelector('body')
       .appendChild(mainDiv);
     return mainDiv;
   }
 
+  setupClearAllBtn() {
+    const button = document.createElement('button');
+    button.style.cssText = 'padding:0 3px;border:1px solid;cursor:pointer';
+    button.title = 'Clear all rows';
+    button.innerText = 'Clear all';
+    button.onclick = () => {
+      while (this.bodydiv.firstChild) {
+        this.bodydiv.removeChild(this.bodydiv.firstChild);
+      }
+    };
+    return button;
+  }
+
+  /*
+  * END MAIN HTML ELEMENTS
+  */
+
+  /**
+   * Sets up the dialog draging capabilites
+   * @param {HTMLElement} headerDiv an element to be the drag-enabled one
+   */
   setupDialogDrag(headerDiv) {
     this.vpH = document.documentElement.clientHeight;
     this.vpW = document.documentElement.clientWidth;
@@ -171,7 +374,7 @@ class FbLinkCleaner {
   hideDialog() {
     this.main.style.visibility = 'hidden';
     this.main.style.opacity = '0';
-    FbLinkCleaner.unregisterEventListeners();
+    this.unregisterEventListeners();
   }
 
   /**
@@ -183,22 +386,22 @@ class FbLinkCleaner {
     mainDivStyle.opacity = '1';
     mainDivStyle.top = '0';
     mainDivStyle.left = '0';
-    FbLinkCleaner.registerEventListeners();
+    this.registerEventListeners();
   }
 
   /**
    * Registers event listeners for mousedown and click
    */
-  static registerEventListeners() {
-    document.addEventListener('mousedown', FbLinkCleaner.getOriginalLinkOnMiddleClick);
+  registerEventListeners() {
+    document.addEventListener('mousedown', this.getOriginalLinkOnMiddleClick.bind(this));
     document.addEventListener('click', FbLinkCleaner.cancelClickOnMiddleClick);
   }
 
   /**
    * Unregisters event listeners for mousedown and click
    */
-  static unregisterEventListeners() {
-    document.removeEventListener('mousedown', FbLinkCleaner.getOriginalLinkOnMiddleClick);
+  unregisterEventListeners() {
+    document.removeEventListener('mousedown', this.getOriginalLinkOnMiddleClick.bind(this));
     document.removeEventListener('click', FbLinkCleaner.cancelClickOnMiddleClick);
   }
 }
